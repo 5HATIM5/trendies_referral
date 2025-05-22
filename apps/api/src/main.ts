86a7ab8034ae1d48151ab96e41a3f27c53bb864d
Hatim Shabbir <hatim.shabbir5152@gmail.com>
2025-05-22 23:15:55 +0200

    Update (web) - CORS URL update from local to prod

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: "http://localhost:3000",
+    origin: "https://trendies-referral-web.vercel.app/",
   });
 
   await app.listen(process.env.PORT ?? 3001);

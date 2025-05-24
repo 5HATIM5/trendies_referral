5c28dfdac9e7a8e747131af463a26a7d8f3d09d5
Hatim Shabbir <hatim.shabbir5152@gmail.com>
2025-05-24 02:29:31 +0200

    Fix (web) - Dashboard Middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,19 +1,16 @@
-// apps/web/middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('accessToken')?.value;
 
-  // If user is not logged in and tries to access protected route
   if (!token && request.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/', request.url)); // redirect to home/login
+    return NextResponse.redirect(new URL('/', request.url)); 
   }
 
   return NextResponse.next(); // allow the request
 }
 
-// Tell Next.js which paths to run this on
 export const config = {
-  matcher: ['/dashboard'], // protect everything under /dashboard
+  matcher: ['/dashboard'],
 };
--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -1,18 +1,10 @@
 import Navbar from "../components/Global/Navbar";
-import { redirect } from 'next/navigation';
-import { cookies } from 'next/headers';
 
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-
-  const token = (await cookies()).get('accessToken')?.value;
-  
-  if (!token) {
-    redirect('/');
-  }
 
   return (
     <>

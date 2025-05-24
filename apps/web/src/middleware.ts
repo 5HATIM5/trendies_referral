37fb7e372fa1d6a9099d5ed28cde2f31a576d0e5
Hatim Shabbir <hatim.shabbir5152@gmail.com>
2025-05-24 03:42:39 +0200

    Update (web) - Middleware removed not working in prod

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,16 +1,16 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+// import { NextResponse } from 'next/server';
+// import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get('accessToken')?.value;
-  // Use request.nextUrl.origin to build redirect URLs
-  if (!token && request.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/', request.nextUrl.origin));
-  }
+// export function middleware(request: NextRequest) {
+//   const token = request.cookies.get('accessToken')?.value;
+//   // Use request.nextUrl.origin to build redirect URLs
+//   if (!token && request.nextUrl.pathname.startsWith('/dashboard')) {
+//     return NextResponse.redirect(new URL('/', request.nextUrl.origin));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ['/dashboard/:path*'],
-};
+// export const config = {
+//   matcher: ['/dashboard/:path*'],
+// };
